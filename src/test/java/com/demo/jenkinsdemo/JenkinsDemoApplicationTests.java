2d8edd70bd60af75711035203dbb51dc1403bc44
Darren Fang <2055563+darrenfang@users.noreply.github.com>
2024-11-21 23:49:15 +0800

    update

---
+++ src/test/java/com/demo/jenkinsdemo/JenkinsDemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.demo.jenkinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
